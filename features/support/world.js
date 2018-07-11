aa739a1195b7798bd10662d0e11207e32c243865
oliver_ochman <oliver.ochman@hotmail.com>
2018-07-11 10:00:09 +0200

    add header to view

--- features/support/world.js
+++ features/support/world.js
@@ -15,7 +15,14 @@ class AdressBook {
   }
 
   async  closeHomePage()  {
-   await  this.browser.close()
+    await  this.browser.close()
+  }
+
+  async pageHasTextContent(expectedContent) {
+    const pageContent = await this.page.content();
+    const actualContent = pageContent.match(expectedContent)[0];
+
+    expect(actualContent).to.be.eq(expectedContent);
   }
 }
 
